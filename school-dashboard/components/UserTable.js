a21986e0de592cf7ee0e5f85d917433ce3c2ef3b
mysticfalconvt <rboskind@gmail.com>
2021-03-01 16:06:49 -0500

    added basic table for user page

--- school-dashboard/components/UserTable.js
+++ school-dashboard/components/UserTable.js
@@ -1,7 +1,74 @@
-export default function UserTable({ users }) {
+import { useState } from 'react';
+import { useTable, useFilters, useSortBy } from 'react-table';
+import { UserTableStyles } from './styles/TableStyles';
+
+export default function Table({ columns, data }) {
+  const [filterInput, setFilterInput] = useState('');
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    setFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useFilters,
+    useSortBy
+  );
+
+  const handleFilterChange = (e) => {
+    const value = e.target.value || undefined;
+    setFilter('name', value);
+    setFilterInput(value);
+  };
+
+  // Render the UI for your table
   return (
-    <div>
-      <p>{JSON.stringify(users)}</p>
-    </div>
+    <UserTableStyles>
+      <input
+        value={filterInput}
+        onChange={handleFilterChange}
+        placeholder="Search name"
+      />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  className={
+                    column.isSorted
+                      ? column.isSortedDesc
+                        ? 'sort-desc'
+                        : 'sort-asc'
+                      : ''
+                  }
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </UserTableStyles>
   );
 }
--- school-dashboard/pages/users.js
+++ school-dashboard/pages/users.js
@@ -1,4 +1,5 @@
 import { gql } from 'graphql-request';
+import { useMemo } from 'react';
 import UserTable from '../components/UserTable';
 import { useGQLQuery } from '../lib/useGqlQuery';
 
@@ -20,12 +21,58 @@ const GET_ALL_USERS = gql`
   }
 `;
 
+const ArrayValues = ({ values }) => (
+  <>
+    {values.map((arrayValue, idx) => (
+      <span key={idx} className="badge">
+        {arrayValue.name}
+      </span>
+    ))}
+  </>
+);
+
 export default function Users() {
   const { data, isLoading, error } = useGQLQuery('users', GET_ALL_USERS);
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Users',
+        columns: [
+          {
+            Header: 'Name',
+            accessor: 'name',
+          },
+          {
+            Header: 'Type',
+            accessor: 'role',
+            Cell: ({ cell: { value } }) => <ArrayValues values={value || []} />,
+          },
+          {
+            Header: 'TA Teacher',
+            accessor: 'taTeacher.name',
+          },
+
+          {
+            Header: 'Callback',
+            accessor: 'callbackCount',
+          },
+          {
+            Header: 'Weekly PBIS',
+            accessor: 'PbisCardCount',
+          },
+          {
+            Header: 'Yearly PBIS',
+            accessor: 'yearPbisCount',
+          },
+        ],
+      },
+    ],
+    []
+  );
   console.log(data);
   return (
     <div>
-      <UserTable users={data.allUsers} />
+      <UserTable data={data?.allUsers || []} columns={columns} />
     </div>
   );
 }
