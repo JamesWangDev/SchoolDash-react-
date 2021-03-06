a9b9756420678248ef375b2663436e4873263378
MysticfalconVT <rboskind@gmail.com>
2021-03-06 16:51:20 -0500

    user page sortable teacher vs student

--- school-dashboard/pages/users.js
+++ school-dashboard/pages/users.js
@@ -1,13 +1,15 @@
 import { gql } from 'graphql-request';
 import Link from 'next/link';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import styled from 'styled-components';
+import DisplayError from '../components/ErrorMessage';
 import GradientButton from '../components/styles/Button';
 import Table from '../components/Table';
 import { useGQLQuery } from '../lib/useGqlQuery';
 
 const GET_ALL_USERS = gql`
-  query GET_ALL_USERS {
-    allUsers {
+  query GET_ALL_USERS($searchTerm: String) {
+    allUsers(where: { role_some: { name: $searchTerm } }) {
       id
       name
       role {
@@ -23,6 +25,26 @@ const GET_ALL_USERS = gql`
   }
 `;
 
+const ButtonStyles = styled.div`
+  /* background: red; */
+
+  button {
+    border: 2px solid white;
+    position: absolute;
+    :hover {
+      border: 2px solid var(--red);
+    }
+  }
+  .hide {
+    opacity: 0;
+    visibility: hidden;
+  }
+  .show {
+    opacity: 1;
+  }
+  padding-bottom: 3.5rem;
+`;
+
 const ArrayValues = ({ values }) => (
   <>
     {values.map((arrayValue, idx) => (
@@ -35,7 +57,13 @@ const ArrayValues = ({ values }) => (
 
 export default function Users() {
   const [userSortType, setUserSortType] = useState('student');
-  const { data, isLoading, error } = useGQLQuery('users', GET_ALL_USERS);
+  const { data, isLoading, error, refetch } = useGQLQuery(
+    'users',
+    GET_ALL_USERS,
+    {
+      searchTerm: userSortType,
+    }
+  );
   const columns = useMemo(
     () => [
       {
@@ -77,17 +105,31 @@ export default function Users() {
     ],
     []
   );
-  // console.log(data);
+
+  useEffect(() => refetch(), [userSortType]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <DisplayError>{error.mesage}</DisplayError>;
   return (
     <div>
-      <div>
-        <GradientButton onClick={() => setUserSortType('teacher')}>
+      <ButtonStyles>
+        <GradientButton
+          onClick={() => {
+            setUserSortType('staff');
+          }}
+          className={userSortType === 'staff' ? 'hide' : 'show'}
+        >
           Show Teachers
         </GradientButton>
-        <GradientButton onClick={() => setUserSortType('student')}>
-          Show Parents
+        <GradientButton
+          onClick={() => {
+            setUserSortType('student');
+          }}
+          className={userSortType === 'student' ? 'hide' : 'show'}
+        >
+          Show Students
         </GradientButton>
-      </div>
+      </ButtonStyles>
       <Table
         data={data?.allUsers || []}
         columns={columns}
