c5169743dc90e79ba47cade40a7312c5eba3f5fe
MysticfalconVT <rboskind@gmail.com>
2021-03-16 16:29:53 -0400

    pbis card mutation calculates for student

--- BackEnd/mutations/recalculatePBIS.ts
+++ BackEnd/mutations/recalculatePBIS.ts
@@ -3,15 +3,36 @@ import { Session } from '../types';
 import { UserUpdateInput } from '../.keystone/schema-types';
 
 
+const graphql = String.raw;
+
 async function recalculatePbis( root: any,
     { userId }: { userId: string },
     context: KeystoneContext
   ): Promise<UserUpdateInput> {
     console.log('Updating PBIS Count');
     console.log(userId)
+    const student = await context.lists.User.findOne({
+        where: { id: userId },
+        resolveFields: graphql`
+            id
+             cards: _studentPbisCardsMeta{
+             count
+            }
+             yearCards: _studentPbisCardsMeta(where:{counted:false}){
+             count
+            }
+            taTeacher{
+        id
+    }
+        `,
+      });
+      console.log(student)
     return await context.lists.User.updateOne({
         id: userId,
-        data: { PbisCardCount: 666 },
+        data: { 
+            PbisCardCount: student.cards.count,
+            YearPbisCount: student.yearCards.count
+         },
         resolveFields: false,
       });
 }
