72197a25220d84e79ef1e66a2922d9823fcf1bb9
MysticfalconVT <rboskind@gmail.com>
2021-03-30 20:28:50 -0400

    pbis card fade in

--- school-dashboard/components/PBIS/PbisCardFormButton.js
+++ school-dashboard/components/PBIS/PbisCardFormButton.js
@@ -2,22 +2,37 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import GradientButton from '../styles/Button';
 import SearchForUserName from '../SearchForUserName';
+import Form from '../styles/Form';
 
 const CardButtonContainer = styled.div`
   padding: 20px;
   display: flex;
+  transition: ease-in-out 2s;
 `;
 
 const CardFormContainerStyles = styled.div`
-  transition: ease-out 2s;
+  .visible {
+    /* background: red; */
+    opacity: 1;
+    transition: all ease-in-out 1s;
+  }
+  .invisible {
+    opacity: 0;
+    transition: all ease-out 1s;
+  }
 `;
 
-function CardForm() {
-  // console.log('cardForm');
+function CardForm({ visible }) {
+  console.log(visible);
   return (
     <CardFormContainerStyles>
-      <p>Form here</p>
-      <SearchForUserName name="studentName" />
+      <div className={visible}>
+        <Form>
+          <p>Form here</p>
+
+          <SearchForUserName name="studentName" />
+        </Form>
+      </div>
     </CardFormContainerStyles>
   );
 }
@@ -34,7 +49,7 @@ export default function PbisCardFormButton({ teacher }) {
       >
         PBIS CARD
       </GradientButton>
-      {displayCardForm && <CardForm />}
+      <CardForm visible={displayCardForm ? 'visible' : 'invisible'} />
     </CardButtonContainer>
   );
 }
