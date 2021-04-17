7576ba28a385f2b3afe223ef8ae391001bf30bba
MysticfalconVT <rboskind@gmail.com>
2021-04-17 11:08:17 -0400

    updated endpoint for production

--- school-dashboard/config.js
+++ school-dashboard/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://school-dashboard-mk-ii.herokuapp.com/`;
+export const prodEndpoint = `https://api.ncujhs.tech/api/graphql`;
 export const perPage = 4;
