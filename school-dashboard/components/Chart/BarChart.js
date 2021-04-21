fb37a05bb015b4a33ca176e11ecfcd0b1cd9f90c
MysticfalconVT <rboskind@gmail.com>
2021-04-21 10:21:50 -0400

    added day of teh week to discipline charts

--- school-dashboard/components/Chart/BarChart.js
+++ school-dashboard/components/Chart/BarChart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Bar } from '@reactchartjs/react-chart.js';
+import { backgroundColors, borderColors } from './chartColors';
 
 const options = {
   scales: {
@@ -7,9 +8,15 @@ const options = {
       {
         ticks: {
           beginAtZero: true,
+          display: false,
         },
-
-        display: false,
+      },
+    ],
+    xAxes: [
+      {
+        ticks: {
+          display: false,
+        },
       },
     ],
   },
@@ -33,22 +40,8 @@ export default function BarChart({ title, chartData }) {
       {
         label: '# of Votes',
         data: dataToChart,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
+        backgroundColor: backgroundColors,
+        borderColor: borderColors,
         borderWidth: 1,
       },
     ],
--- school-dashboard/components/discipline/DisciplineCharts.js
+++ school-dashboard/components/discipline/DisciplineCharts.js
@@ -12,6 +12,41 @@ import DoughnutChart from '../Chart/DonutChart';
 import BarChart from '../Chart/BarChart';
 import totalsTrueInArray from '../../lib/totalsTrueInArray';
 
+const weekday = new Array(7);
+weekday[0] = 'Sunday';
+weekday[1] = 'Monday';
+weekday[2] = 'Tuesday';
+weekday[3] = 'Wednesday';
+weekday[4] = 'Thursday';
+weekday[5] = 'Friday';
+weekday[6] = 'Saturday';
+
+const days = new Array(7);
+days[0] = 0;
+days[1] = 1;
+days[2] = 2;
+days[3] = 3;
+days[4] = 4;
+days[5] = 5;
+days[6] = 6;
+
+function getDayTotals(disciplines) {
+  const arrayOfDayOfTheWeek = disciplines.map((individual) => {
+    const day = new Date(individual.date).getDay();
+    return day;
+  });
+  // console.log(arrayOfDayOfTheWeek);
+  const counts = days.map((day) =>
+    arrayOfDayOfTheWeek.reduce((total, currentDay) => {
+      const isThatDay = currentDay === day;
+
+      return total + (isThatDay ? 1 : 0);
+    }, 0)
+  );
+  return counts.map((total, index) => ({ word: weekday[index], total }));
+  // return counts;
+}
+
 export default function DisciplineCharts({ disciplines }) {
   const totalDisciplines = disciplines.length;
   const classList = classTypeList;
@@ -21,10 +56,13 @@ export default function DisciplineCharts({ disciplines }) {
   const conducts = totalsTrueInArray(studentConductList, disciplines);
   const teacherActions = totalsTrueInArray(teacherActionList, disciplines);
   const others = totalsTrueInArray(othersInvolvedList, disciplines);
+  const dates = getDayTotals(disciplines);
+  console.log(dates);
   return (
     <div>
       <h3>chart goes here</h3>
       <p>There are a total of {totalDisciplines} referrals</p>
+      <DoughnutChart title="Day of the Week" chartData={dates} />
       <DoughnutChart title="Class Type" chartData={totalPerClass} />
       <DoughnutChart title="Location" chartData={locations} />
       <DoughnutChart title="Time Of Day" chartData={times} />
