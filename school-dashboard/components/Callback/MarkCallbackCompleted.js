1094519e5a8782ee78e95a0dd0e7610bbe08e8e1
MysticfalconVT <rboskind@gmail.com>
2021-06-24 08:53:53 -0400

    working complete callback button

--- school-dashboard/components/Callback/MarkCallbackCompleted.js
+++ school-dashboard/components/Callback/MarkCallbackCompleted.js
@@ -1,11 +1,17 @@
+import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
+import { useQueryClient } from 'react-query';
 import { useUser } from '../User';
 
 const MARK_CALLBACK_COMPLETED = gql`
-  mutation($id: ID!, $dateCompleted: String!) {
+  mutation MARK_CALLBACK_COMPLETED(
+    $id: ID!
+    $dateCompleted: String!
+    $daysLate: Int!
+  ) {
     updateCallback(
-      id: "60d313224ed33bf4adc86e91"
-      data: { dateCompleted: "12/12/2021" }
+      id: $id
+      data: { dateCompleted: $dateCompleted, daysLate: $daysLate }
     ) {
       id
     }
@@ -13,15 +19,36 @@ const MARK_CALLBACK_COMPLETED = gql`
 `;
 
 export default function MarkCallbackCompleted({ callback }) {
+  const queryClient = useQueryClient();
   const me = useUser();
   const today = new Date();
   const dateAssigned = new Date(callback.dateAssigned);
   const daysLate = Math.round((today - dateAssigned) / 1000 / 60 / 60 / 24);
+  const [markCompleted, { loading, error }] = useMutation(
+    MARK_CALLBACK_COMPLETED,
+    {
+      variables: {
+        id: callback.id,
+        dateCompleted: today.toISOString(),
+        daysLate,
+      },
+    }
+  );
+
   //   console.log(`late: ${daysLate}`);
   if (me.id === callback.teacher.id) {
     return (
       <div>
-        <button type="button">Mark Completed {daysLate} Days overdue</button>
+        <button
+          type="button"
+          onClick={async () => {
+            console.log('marking completed');
+            await markCompleted();
+            queryClient.refetchQueries();
+          }}
+        >
+          Mark Completed {daysLate} Days overdue
+        </button>
       </div>
     );
   }
