b4e19354c67fff7f6383a76c6120ef252c2f2430
MysticfalconVT <rboskind@gmail.com>
2021-07-13 16:07:35 -0400

    added falcon needs styling

--- school-dashboard/components/PBIS/PbisFalcon.js
+++ school-dashboard/components/PBIS/PbisFalcon.js
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag';
 import React from 'react';
+import styled from 'styled-components';
 import { useGQLQuery } from '../../lib/useGqlQuery';
 import DisplayError from '../ErrorMessage';
 import Loading from '../Loading';
@@ -13,6 +14,38 @@ const TOTAL_PBIS_CARDS = gql`
   }
 `;
 
+const ContainerStyles = styled.div`
+  height: 150px;
+  width: 150px;
+  background-color: var(--red);
+  border-radius: 50;
+  margin: 50;
+  /* border-radius: 1rem; */
+
+  .filler {
+    width: 100%;
+    height: ${({ percentageFull }) => 100 - percentageFull * 200}%;
+    background-color: var(--blue);
+    position: relative;
+    bottom: 0px;
+    transition: width 1s ease-in-out;
+    text-align: right;
+    /* border-radius: 1rem 1rem 0rem 0rem; */
+  }
+
+  .label {
+    padding: 5;
+    color: white;
+    font-weight: bold;
+  }
+  img {
+    position: relative;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
 export default function PbisFalcon() {
   const { data, isLoading, error } = useGQLQuery(
     'totalPbisCards',
@@ -21,10 +54,18 @@ export default function PbisFalcon() {
 
   if (isLoading) return <Loading />;
   if (error) return <DisplayError error={error} />;
-  const percentageFull = (data._allPbisCardsMeta.count / 34573) * 100;
+  const percentageFull =
+    Math.round((data._allPbisCardsMeta.count / 34573) * 10000) / 100;
 
   return (
     <div>
+      <ContainerStyles percentageFull={percentageFull}>
+        <div className="filler">
+          <img src="/falcon.svg" />
+          <span className="label">{`${percentageFull}%`}</span>
+        </div>
+      </ContainerStyles>
+
       <p>falcon goes here</p>
       <p>{data._allPbisCardsMeta.count}</p>
       <p>{percentageFull}%</p>
