e155dc5c2ceb9fdda9c51528a5375c79a62ba455
MysticfalconVT <rboskind@gmail.com>
2021-07-13 13:28:44 -0400

    added bulk calendar update

--- school-dashboard/pages/superUserSettings.js
+++ school-dashboard/pages/superUserSettings.js
@@ -1,5 +1,6 @@
 import StudentFocusTable from '../components/StudentFocusTable';
 import { useUser } from '../components/User';
+import NewEvents from '../components/users/NewEvents';
 import NewStaff from '../components/users/NewStaff';
 import NewUpdateUsers from '../components/users/NewUpdateUsers';
 import isAllowed from '../lib/isAllowed';
@@ -11,6 +12,7 @@ export default function superUserSettings() {
       <h1>Administration</h1>
       {isAllowed(me, 'isSuperAdmin') && <NewUpdateUsers />}
       {isAllowed(me, 'isSuperAdmin') && <NewStaff />}
+      {isAllowed(me, 'isSuperAdmin') && <NewEvents />}
     </div>
   );
 }
