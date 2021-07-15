96c173902e5a513802790b881f78219531b61f6b
MysticfalconVT <rboskind@gmail.com>
2021-07-15 08:07:23 -0400

    still working on logging ta cards

--- school-dashboard/components/PBIS/CountPhysicalCards.js
+++ school-dashboard/components/PBIS/CountPhysicalCards.js
@@ -6,37 +6,55 @@ import useForm from '../../lib/useForm';
 import DisplayError from '../ErrorMessage';
 import GradientButton from '../styles/Button';
 import Form, { FormContainerStyles } from '../styles/Form';
+import { useUser } from '../User';
 
 const CREATE_CARD_MUTATION = gql`
-  mutation CREATE_CARD_MUTATION($studentScheduleData: String!) {
-    addStaff(staffData: $studentScheduleData) {
-      name
+  mutation CREATE_CARD_MUTATION($cards: [PbisCardsCreateInput]) {
+    createPbisCards(data: $cards) {
+      id
     }
   }
 `;
 
+// function to create array of cards based on value of id
+function createCards(users, teacherId) {
+  const userToAddCards = Object.keys(users);
+  const cards = [];
+  userToAddCards.forEach((user) => {
+    // console.log(users);
+    if (users[user] > 0) {
+      const numberOfCards = users[user];
+      for (let i = 0; i < numberOfCards; i++) {
+        cards.push({
+          data: {
+            student: { connect: { id: user } },
+            teacher: { connect: { id: teacherId } },
+            category: 'physical',
+          },
+        });
+      }
+    }
+  });
+  //   console.log(cards);
+  return cards;
+}
 export default function CountPhysicalCards({ taStudents }) {
+  const me = useUser();
   const [showForm, setShowForm] = useState(false);
 
   // creat an array of objects with keys taStudent.id and value of 0
-  const taStudentCounts = [];
+  const taStudentCounts = {};
   taStudents.forEach((student) => {
-    taStudentCounts.push({
-      key: student.id,
-      value: 0,
-    });
+    taStudentCounts[student.id] = 0;
   });
   // create an object with key of taStudent.id and value of 0
 
   //   const studentIds = taStudents.map((student) => student.id);
-  console.log(taStudentCounts);
-  const { inputs, handleChange, clearForm } = useForm();
+  const { inputs, handleChange, clearForm } = useForm(taStudentCounts);
   const [countCardsMutation, { loading, error, data }] = useMutation(
-    CREATE_CARD_MUTATION,
-    {
-      variables: { studentScheduleData: inputs.userData },
-    }
+    CREATE_CARD_MUTATION
   );
+  //   console.log(inputs);
 
   return (
     <div>
@@ -50,34 +68,47 @@ export default function CountPhysicalCards({ taStudents }) {
         <FormContainerStyles>
           <Form
             className={showForm ? 'visible' : 'hidden'}
+            style={{ width: 'max-content' }}
             onSubmit={async (e) => {
               e.preventDefault();
               // Submit the inputfields to the backend:
               //   const res = await countCardsMutation();
-              console.log(inputs);
-
-              // clearForm();
-              setShowForm(false);
+              //   console.log(inputs);
+              const cardsToCreate = await createCards(inputs, me.id);
+              console.log(JSON.stringify(cardsToCreate));
+              //   const res = await countCardsMutation({
+              //     variables: { data: inputs },
+              //   });
+              //   console.log(res);
+              clearForm();
+              //   setShowForm(false);
             }}
           >
-            <h1>Update all students schedules</h1>
+            <h1>Log TA Cards</h1>
             <DisplayError error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
               {taStudents.map((student) => (
-                <div key={student.id}>
-                  <label htmlFor={student.id}>
+                <div
+                  key={student.id}
+                  style={{
+                    // display: 'inline',
+                    // width: '5rem',
+                    marginLeft: '10px',
+                    display: 'grid',
+                    gridTemplateColumns: '50% 5rem',
+                  }}
+                >
+                  <label htmlFor={student.id} style={{ display: 'inline' }}>
                     {student.name}
-                    <span>
-                      <input
-                        type="number"
-                        id={student.id}
-                        name={student.id}
-                        placeholder="Enter number of cards"
-                        value={inputs[student.id]}
-                        onChange={handleChange}
-                      />
-                    </span>
                   </label>
+                  <input
+                    type="number"
+                    id={student.id}
+                    name={student.id}
+                    placeholder="Enter number of cards"
+                    value={inputs[student.id]}
+                    onChange={handleChange}
+                  />
                 </div>
               ))}
 
