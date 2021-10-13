0e6c96e28abed14dbf8b1533401d382668a394a5
mysticfalconvt <rboskind@gmail.com>
2021-10-13 10:33:48 -0400

    bullying date sort error fixed

--- school-dashboard/pages/Bullying.js
+++ school-dashboard/pages/Bullying.js
@@ -29,7 +29,7 @@ const BullyingPageContainer = styled.div`
 
 const BULLYING_DATA_QUERY = gql`
   query BULLYING_DATA_QUERY {
-    allBullyings(sortBy: date_DESC) {
+    allBullyings(sortBy: dateReported_DESC) {
       id
       studentOffender {
         id
