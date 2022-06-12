af42d82af0ebe040db9b1fa8d12c725474de748e
jameswang <james.wang.dev712@gmail.com>
2022-06-12 16:08:45 -0400

    added basic command pallet

---
+++ school-dashboard/lib/CommandPallettePaths.js
@@ -0,0 +1,62 @@
+export const commandPallettePaths = [
+    {
+        id: 'links',
+        name: 'Links',
+        icon: '🔗',
+        path: '/links',
+    },
+    {
+        id: 'calendar',
+        name: 'Calendar',
+        icon: '📆',
+        path: '/calendar',
+    },
+    {
+        id: 'callback',
+        name: 'Callback',
+        icon: '📥',
+        path: '/callback',
+    },
+    {
+        id: 'users',
+        name: 'Users',
+        icon: '👤',
+        path: '/users',
+    },
+    {
+        id: 'pbis',
+        name: 'PBIS',
+        icon: '🪪',
+        path: '/pbis',
+    },
+    {
+        id: 'discipline',
+        name: 'Discipline',
+        icon: '📝',
+        path: '/discipline',
+    },
+    {
+        id: "studentFocus",
+        name: "Student Focus",
+        icon: "📝",
+        path: "/studentFocus",
+    },
+    {
+        id: 'allTeacherCurrentWork',
+        name: 'All Teacher Current Work',
+        icon: '📝',
+        path: '/allTeacherCurrentWork',
+    },
+    {
+        id: 'Bullying',
+        name: 'Hazing Harassment Bullying',
+        icon: '😢',
+        path: '/Bullying',
+    },
+    {
+        id: 'home',
+        name: 'Home',
+        icon: '🏠',
+        path: '/',
+    },
+]
